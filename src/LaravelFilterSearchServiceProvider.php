9946ca17922cdd6077e3652fb71f24536c4f8118
théoo <90846584+theokbokki@users.noreply.github.com>
2024-11-11 22:17:30 +0100

    initial commit

---
+++ src/LaravelFilterSearchServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Vendor\Package;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelFilterSearchServiceProvider extends ServiceProvider
+{
+    public function register(): void 
+    {}
+
+    public function boot(): void 
+    {}
+}
